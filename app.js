13d2ccc13b8844a59b5b8c7821ef0af01cd9f729
SantiagoMurilloV <128425308+SantiagoMurilloV@users.noreply.github.com>
2024-04-05 10:28:02 -0500

    Changes were made to test and improve the API, and cors was added to receive .json and be able to read and store them in the db

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors'); 
 const sensorRoutes = require('./src/routes/sensorRoutes');
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+
+app.use(cors()); 
+app.use(express.json()); 
 
 mongoose.connect(process.env.MONGODB_URL, {
 }).then(() => console.log('Conectado a MongoDB'))
   .catch(err => console.error(err));
 
-app.use(express.json());
 
 app.use('/api', sensorRoutes);
 
