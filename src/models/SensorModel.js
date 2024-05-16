dea1ab4e96ea701156fcdb202581517bf9bfac25
SantiagoMurilloV <128425308+SantiagoMurilloV@users.noreply.github.com>
2024-05-16 05:29:08 -0500

    The API was modified to accept the data sent by the Raspberry Pi 2W.

--- src/models/SensorModel.js
+++ src/models/SensorModel.js
@@ -1,9 +1,19 @@
 const mongoose = require('mongoose');
 
 const sensorSchema = new mongoose.Schema({
-  actual_date: String,
-  air_temperature: Number,
-  air_humidity: Number,
+  actualDate: String,
+  mH: Number, 
+  Pho_7in1: Number,
+  Pot_7in1: Number,
+  Nit_7in1: Number,
+  Moi_7in1: Number, 
+  Tem_7in1: Number,
+  Ph_7in1: Number,
+  Con_7in1:Number
 });
 
 module.exports = mongoose.model('Sensor', sensorSchema);
+
+
+
+
--- src/routes/sensorRoutes.js
+++ src/routes/sensorRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const sensorController = require('../controller/sensorController');
 
-router.post('/sensorsData', sensorController.postSensorData);
+router.post('/sensorsData', sensorController.createSensorData);
 router.get('/data', sensorController.getSensorData);
 
 module.exports = router;
